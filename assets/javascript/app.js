3d97f05be2d3c4a70b553949f8548ffa2507a624
AshSeitz <ash.seitz94@gmail.com>
2018-10-24 12:59:02 -0400

    git commit -m

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,16 @@
+$(document).ready(function(){
 var topics = ["halloween", "jack-o-lanterns", "witches", "ghosts", "monsters"]
 
 // Function for buttons
 function renderButtons() {
 
     $("#buttons").empty();
-    // Loops through the array of movies
+    // Loops through the array
     for (var i = 0; i < topics.length; i++) {
 
         var a = $("<button>");
-        // Adds a class of movie to our button
-        a.addClass("gifs");
+        // Adds a class
+        a.addClass("choices");
         // Added a data-attribute
         a.attr("data-choices", topics[i]);
         // Provided the initial button text
@@ -21,49 +22,57 @@ function renderButtons() {
 renderButtons()
 
 $("button").on("click", function () {
-    
+
     var choices = $(this).attr("data-choices");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         choices + "&api_key=SrVjxeVTjet5ETJi5XiXxSADyYPTZ4XH&limit=10&rating=pg";
-        
+
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
-    
+
             var results = response.data;
             console.log(results)
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
-                
+
                 var rating = results[i].rating;
-                
+
                 var p = $("<p>").text("Rating: " + rating);
 
                 var hallowImage = $("<img>");
-                hallowImage.attr("src", results[i].images.fixed_height_still.url, results[i].images.fixed_height.url);
-
-
+                hallowImage.addClass("gif")
+                hallowImage.attr("src", results[i].images.fixed_height_still.url);
+                hallowImage.attr("data-state", "still")
+                hallowImage.attr("data-still", results[i].images.fixed_height_still.url)
+                hallowImage.attr("data-animate", results[i].images.fixed_height.url)
+              
                 gifDiv.prepend(p);
                 gifDiv.append(hallowImage);
 
                 $("#gifsDisplay").prepend(gifDiv);
             }
+
+            $(".gif").on("click", function () {
+                console.log(".gif")
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"))
+                    $(this).attr("data-state", "animate")
+                }
+            
+                else {
+                    $(this).attr("src", $(this).attr("data-still"))
+                    $(this).attr("data-state", "still")
+                }
+            });
         });
 });
 
 
-$(".gif").on("click", function () {
-    var state = $(this).attr("data-state");
-    if (state === 'still') {
-        $(this).attr("src", $(this).attr("data-animate"))
-        $(this).attr("data-state", "animate")
-      }
 
-      else {
-        $(this).attr("src", $(this).attr("data-still"))
-        $(this).attr("data-state", "still")
-      }
-    });
+
+})
